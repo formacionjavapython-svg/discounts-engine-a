4e5a867a262971bde9c61f7794fb955abc415d3a
A30_Martin Antonio Ortega Yepez <tinmarorye@gmail.com>
2026-03-18 18:07:56 -0600

    se agrega funcion de Hola Mundo, Martin Ortega

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Clase principal para la practica de Java
+ * */
+
+public class Main {
+
+	public static void main(String[] args){
+		//Metodo para imprimir Hello World
+		System.out.println("Hello world!!");
+	}
+
+}
