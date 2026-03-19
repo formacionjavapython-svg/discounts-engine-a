c39c01f79cf3e0b37a65e4c1ae334c6b2067c562
Arturok2 <arturo_k2@hotmail.com>
2026-03-18 20:52:36 -0600

    casi terminado, me faltaron las pruebas

--- src/main/java/domain/model/Money.java
+++ src/main/java/domain/model/Money.java
@@ -6,21 +6,41 @@ public class Money {
     private final double amount;
     private final String currency;
 
+    /**
+     * Recibe el precio y el tipo de moneda para crear el objeto money
+     * @param amount
+     * @param currency
+     */
     public Money(double amount, String currency) {
         if (amount < 0) throw new IllegalArgumentException("El monto no puede ser negativo");
         this.amount = amount;
         this.currency = Objects.requireNonNull(currency);
     }
 
+    /**
+     * 
+     * @param other
+     * @return
+     */
     public Money add(Money other) {
         checkCurrency(other);
         return new Money(this.amount + other.amount, this.currency);
     }
 
+    /**
+     * 
+     * @param quantity
+     * @return
+     */
     public Money multiply(int quantity) {
         return new Money(this.amount * quantity, this.currency);
     }
 
+    /**
+     * 
+     * @param other
+     * @return
+     */
     public Money subtract(Money other) {
         checkCurrency(other);
         return new Money(Math.max(0, this.amount - other.amount), this.currency);
