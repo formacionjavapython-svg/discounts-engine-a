176358eb2ce6099dd76791bde306316405aac1f0
VictorHugoDuranSantiago <victor-duran31@hotmail.com>
2026-03-18 19:23:10 -0600

    Fix: Error on units tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,36 +12,6 @@ public class Main {
     public static void main(String[] args) {
 
         runTests();
-
-        CartService cart = new CartService();
-
-        cart.addItem(new Item("Laptop", Money.of(15000, "MXN"), 1));
-        cart.addItem(new Item("Mouse", Money.of(500, "MXN"), 3));
-
-        System.out.println("Total carrito: " + cart.total());
-
-        List<DiscountRule> rules = List.of(
-                new ThresholdDiscount(Money.of(5000, "MXN"), 0.10),
-                new CouponDiscount("SUPER2024", Money.of(500, "MXN")),
-                new BulkDiscount(3, 0.05)
-        );
-
-        Money totalDiscount = Money.zero(cart.total().getCurrency());
-
-        for (DiscountRule rule : rules) {
-            totalDiscount = totalDiscount.add(rule.apply(cart));
-        }
-
-        Money cartTotal = cart.total();
-
-        if (totalDiscount.getAmount().compareTo(cartTotal.getAmount()) > 0) {
-            totalDiscount = cartTotal;
-        }
-
-        Money finalTotal = cartTotal.subtract(totalDiscount);
-
-        System.out.println("Descuento total: " + totalDiscount);
-        System.out.println("Total final: " + finalTotal);
     }
 
 
