6c588937b0cc28a991a4a7c1e863bcdcfaa1e664
rpicazo20 <rpicazo.techfactory@gmail.com>
2026-03-18 19:34:52 -0600

    Delete src/main/java directory

--- src/main/java/modules/discounts/interfaces/DiscountRule.java
+++
@@ -1,9 +0,0 @@
-package  modules.discounts.interfaces;
-
-
-import modules.discounts.dto.Money;
-import modules.discounts.services.CartService;
-
-public interface DiscountRule {
-    Money apply(CartService cart);
-}
