ef415bcbe219c9a8d484a1fb59faa7a85825d4d3
A30_Martin Antonio Ortega Yepez <tinmarorye@gmail.com>
2026-03-22 18:29:20 -0600

    feat: add secure coupon validation with env variable

--- src/main/java/CouponDiscount.java
+++ src/main/java/CouponDiscount.java
@@ -1,7 +1,34 @@
 package src.main.java;
 
+import java.math.BigDecimal;
+import java.security.MessageDigest;
+
 /**
  * Clase para aplicar reglas de cupoines
  */
 public class CouponDiscount {
+
+    private String cuponUsuario;
+
+    public CouponDiscount(String cupon){
+        this.cuponUsuario = cuponUsuario;
+    }
+
+    public Money apply(Cart carrito){
+        String secreto = System.getenv("DISCOUNT_COUPON_SECRET");
+
+        if (secreto != null && validaCupon(cuponUsuario,secreto)){
+            return carrito.calcularTotal().multiplica(BigDecimal.valueOf(0.15));
+        }
+
+        return new Money(BigDecimal.valueOf(0),"MXN");
+    }
+
+    private boolean validaCupon(String cuponUsuario,String cuponSistema){
+        //No debemos comparar con string, se debe usar el MEssage /Digest
+        //return cuponUsuario.equals(cuponSistema);
+
+        return MessageDigest.isEqual(cuponUsuario.getBytes(), cuponSistema.getBytes());
+    }
+
 }
